d8cb4f48a7137a5eb08cbaef7ffc1c6be6909f36
Ashish Vaid <vaid50@yahoo.com>
2017-10-18 11:21:39 -0600

    can't get propane results to render, only electric results show despite radius adjustment

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,11 +3,11 @@ class SearchController < ApplicationController
 
   def index
 
-    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key#{ENV["NREL_KEY"]}") do |faraday|
+    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{ENV["NREL_KEY"]}") do |faraday|
       faraday.adapter Faraday.default_adapter
     end
 
-    response = @conn.get("")
+    response = @conn.get("fuel_type=LPG,ELEC&zip=80203&radius=6.0")
 
     stations_result = JSON.parse(response.body, symbolize_names: true)
 
