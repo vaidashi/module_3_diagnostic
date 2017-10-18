1e642c0ba46545092bffa68674dcca977175be35
Ashish Vaid <vaid50@yahoo.com>
2017-10-18 10:49:49 -0600

    index method in search controller almost built, need to find right query params

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,13 @@ class SearchController < ApplicationController
 
   def index
 
+    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key#{ENV["NREL_KEY"]}") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = @conn.get("")
+
+    stations_result = JSON.parse(response.body, symbolize_names: true)
 
     @stations = stations_result.map do |station|
       Station.new(station)
